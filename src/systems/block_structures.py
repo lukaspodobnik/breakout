c296c7aa8ea479e806d6768e0e3d7c9840376db6
lukaspodobnik <lukas.podobnik@gmail.com>
2025-10-23 10:37:40 +0200

    Add better block spacing.

--- src/systems/block_structures.py
+++ src/systems/block_structures.py
@@ -1,12 +1,18 @@
+from config import BLOCK_HEIGHT, BLOCK_SPACING, BLOCK_WIDTH
 from game_elements.block import Block
 
 
 def square_3x3(position: tuple[int, int]) -> None:
-    for i in range(1, 4):
-        for j in range(1, 4):
-            Block(pos_x=j * position[0], pos_y=i * position[1])
+    x, y = position
+    for i in range(3):
+        for j in range(3):
+            Block(
+                pos_x=(j * (BLOCK_WIDTH + BLOCK_SPACING)) + x,
+                pos_y=(i * (BLOCK_HEIGHT + BLOCK_SPACING)) + y,
+            )
 
 
 def line_1x10(position: tuple[int, int]) -> None:
-    for i in range(1, 11):
-        Block(pos_x=i * position[0], pos_y=position[1])
+    x, y = position
+    for i in range(10):
+        Block(pos_x=(i * (BLOCK_WIDTH + BLOCK_SPACING)) + x, pos_y=y)
